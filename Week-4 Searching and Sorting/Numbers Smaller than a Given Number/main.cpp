3c9ab1cd597fc05cb5e5afe3d146785476c0988c
Prajwalk09 <89776323+Prajwalk09@users.noreply.github.com>
2023-10-24 16:24:51 +0530

    Add files via upload

---
+++ Week-4 Searching and Sorting/Numbers Smaller than a Given Number/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> count (101, 0);
+
+        for(int i = 0 ; i<nums.size(); ++i)
+            count[nums[i]]+=1;
+
+        for(int i = 1 ; i<count.size() ; ++i)
+            count[i] +=count[i-1];
+
+        for(int i =0; i<nums.size(); ++i)
+        {
+            if(nums[i] != 0)
+                nums[i] = count[nums[i] - 1];
+        }
+
+        return nums;
+
+    }
+};
+
+int main()
+{
+    vector<int> nums = {8,1,2,2,3};
+    Solution obj;
+    vector<int> result = obj.smallerNumbersThanCurrent(nums);
+    for(auto it: result)
+        cout<<it<<" ";
+    return 0;
+}
