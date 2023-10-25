4c176b8da8b3ae569c7ec10b4d5c3991e124440f
Prajwalk09 <89776323+Prajwalk09@users.noreply.github.com>
2023-10-25 22:07:20 +0530

    Add files via upload

---
+++ Week-4 Searching and Sorting/Minimum Sum of 4 Digit Number after Splitting/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumSum(int num)
+    {
+        vector<int> numbers;
+        while(num!=0)
+        {
+            int remainder  = num%10;
+            num/=10;
+            numbers.push_back(remainder);
+        }
+        sort(numbers.begin(), numbers.end());
+
+        int new_number_first, new_number_second, minimum_sum;
+
+        new_number_first = numbers[0]*10 + numbers[2];
+        new_number_second = numbers[1]*10 + numbers[3];
+
+        minimum_sum = new_number_first + new_number_second;
+
+        return minimum_sum;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    cout<<obj.minimumSum(2932);
+    return 0;
+}
