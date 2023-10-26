24ee504c9bc6b20171fc44dbec60f2d6f5383d09
Prajwalk09 <89776323+Prajwalk09@users.noreply.github.com>
2023-10-26 21:52:06 +0530

    Update main.cpp

--- Week-4 Searching and Sorting/Sort Array by Parity/main.cpp
+++ Week-4 Searching and Sorting/Sort Array by Parity/main.cpp
@@ -4,62 +4,23 @@ using namespace std;
 
 /* Question link: https://leetcode.com/problems/sort-array-by-parity/ */
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums)
-    {
+    vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int> odd_even;
-        for(auto it: nums)
-        {
+        for(auto it: nums){
             if(it%2 == 0)
                 odd_even.push_back(it);
         }
-        for(auto it: nums)
-        {
+        for(auto it: nums){
             if(it%2 == 1)
                 odd_even.push_back(it);
         }
-        return odd_even;
+    return odd_even;
     }
 };
 
-int main()
-{
-    Solution obj;
-    vector<int> arr = {3,1,2,4};
-    vector<int> result = obj.sortArrayByParity(arr);
-    for(auto it: result)
-        cout<<it<<" ";
-}
-#include<iostream>
-#include<vector>
-using namespace std;
-
-/* Question link: https://leetcode.com/problems/sort-array-by-parity/ */
-
-class Solution
-{
-public:
-    vector<int> sortArrayByParity(vector<int>& nums)
-    {
-        vector<int> odd_even;
-        for(auto it: nums)
-        {
-            if(it%2 == 0)
-                odd_even.push_back(it);
-        }
-        for(auto it: nums)
-        {
-            if(it%2 == 1)
-                odd_even.push_back(it);
-        }
-        return odd_even;
-    }
-};
-
-int main()
-{
+int main(){
     Solution obj;
     vector<int> arr = {3,1,2,4};
     vector<int> result = obj.sortArrayByParity(arr);
