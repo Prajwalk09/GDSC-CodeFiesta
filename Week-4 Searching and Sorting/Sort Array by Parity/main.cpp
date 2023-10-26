2c16d5e0d9dfc8106aa36298fd098188bc7605d8
Prajwalk09 <89776323+Prajwalk09@users.noreply.github.com>
2023-10-26 21:51:09 +0530

    Add files via upload

---
+++ Week-4 Searching and Sorting/Sort Array by Parity/main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/* Question link: https://leetcode.com/problems/sort-array-by-parity/ */
+
+class Solution
+{
+public:
+    vector<int> sortArrayByParity(vector<int>& nums)
+    {
+        vector<int> odd_even;
+        for(auto it: nums)
+        {
+            if(it%2 == 0)
+                odd_even.push_back(it);
+        }
+        for(auto it: nums)
+        {
+            if(it%2 == 1)
+                odd_even.push_back(it);
+        }
+        return odd_even;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<int> arr = {3,1,2,4};
+    vector<int> result = obj.sortArrayByParity(arr);
+    for(auto it: result)
+        cout<<it<<" ";
+}
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/* Question link: https://leetcode.com/problems/sort-array-by-parity/ */
+
+class Solution
+{
+public:
+    vector<int> sortArrayByParity(vector<int>& nums)
+    {
+        vector<int> odd_even;
+        for(auto it: nums)
+        {
+            if(it%2 == 0)
+                odd_even.push_back(it);
+        }
+        for(auto it: nums)
+        {
+            if(it%2 == 1)
+                odd_even.push_back(it);
+        }
+        return odd_even;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<int> arr = {3,1,2,4};
+    vector<int> result = obj.sortArrayByParity(arr);
+    for(auto it: result)
+        cout<<it<<" ";
+}
