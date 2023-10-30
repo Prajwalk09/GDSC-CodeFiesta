1492198c52b8ad56f1e04ea417abd10eb2d26f7e
Prajwalk09 <89776323+Prajwalk09@users.noreply.github.com>
2023-10-30 22:34:37 +0530

    Add files via upload

---
+++ Week-5 Binary Search/Count Negative Numbers in a Sorted Matrix/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    int countNegatives(vector<vector<int>>& grid)
+    {
+        int start, end, mid, count  = 0;
+        for(int i = 0 ; i<grid.size(); ++i)
+        {
+            start = 0, end = grid[i].size()-1;
+            while(start<=end)
+            {
+                mid = (start+end)/2;
+                if(grid[i][mid]>=0)
+                    start = mid+1;
+                else
+                {
+                    count+=end-mid+1;
+                    end = mid-1;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+    Solution obj;
+    cout<<obj.countNegatives(grid)<<endl;
+    return 0;
+}
